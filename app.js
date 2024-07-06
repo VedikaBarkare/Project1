f1a4946b8394392cad3884b9a5f339d9c99f679b
Vedika Barkare <vnbarkare@gmail.com>
2024-07-06 18:16:34 +0530

    Modified js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// let h2 = document.querySelector("h2");
+let h2 = document.querySelector("h2");
 
-// h2.addEventListener("keypress",()=>{
-//     let level
-//     h2.innerText = ;
-// })
+h2.addEventListener("keypress",()=>{
+    let level
+    
+})
